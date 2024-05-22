549d5186d3680ce24356728f6489b0ec6d51b13f
yntha <126660548+yntha@users.noreply.github.com>
2024-05-22 13:59:39 -0400

    Add serializer to the list of module-level classes

--- datastream/__init__.py
+++ datastream/__init__.py
@@ -1,4 +1,5 @@
 from datastream.base import ByteOrder
 from datastream.deserializing import DeserializingStream
+from datastream.serializing import SerializingStream
 
-__all__ = ["ByteOrder", "DeserializingStream"]
+__all__ = ["ByteOrder", "DeserializingStream", "SerializingStream"]
