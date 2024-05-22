96b897637c994a69832ce041cdfd7b5c358d1a58
yntha <126660548+yntha@users.noreply.github.com>
2024-05-22 11:12:33 -0400

    Remove redundant test from `test_stream_read`

--- tests/test_read.py
+++ tests/test_read.py
@@ -9,20 +9,12 @@ def test_stream_read():
     iostream = io.BytesIO()
 
     iostream.write(bytes.fromhex("FF FF FF FF"))
-    iostream.write(bytes.fromhex("FF FF FF FF"))
-    iostream.write(bytes.fromhex("FF FF"))
-    iostream.write(bytes.fromhex("FF"))
 
     iostream.seek(0)
 
     # ensure that the DeserializingStream class accepts buffers and streams alike
     stream = DeserializingStream(iostream)
-    stream = DeserializingStream(iostream.getvalue())
-
-    assert stream.read_int32() == -1
-    assert stream.read_uint32() == 0xFFFFFFFF
-    assert stream.read_int16() == -1
-    assert stream.read_uint8() == 0xFF
+    stream = DeserializingStream(iostream.getvalue())  # noqa: F841
 
 
 def test_stream_read_format():
