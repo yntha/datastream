879b5155a78f2980394190f0c7c246679c7d484d
yntha <126660548+yntha@users.noreply.github.com>
2024-05-26 16:17:11 -0400

    Update list of easy access classes

--- datastream/__init__.py
+++ datastream/__init__.py
@@ -1,5 +1,6 @@
 from datastream.base import ByteOrder
 from datastream.deserializing import DeserializingStream
 from datastream.serializing import SerializingStream
+from datastream.twoway import TwoWayStream
 
-__all__ = ["ByteOrder", "DeserializingStream", "SerializingStream"]
+__all__ = ["ByteOrder", "DeserializingStream", "SerializingStream", "TwoWayStream"]
